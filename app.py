97e35631e8df9f97f7bbfa5425874617cf80c117
Himanshu Moyal <himanshu.moyal@celebaltech.com>
2025-06-11 07:06:02 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from datetime import datetime
 
@@ -64,4 +65,5 @@ def hello():
     '''
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 2000))  # fallback to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
