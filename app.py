9b9b622dae9df31e610ce318f951255415580f09
Himanshu Moyal <himanshu.moyal@celebaltech.com>
2025-06-11 10:58:46 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -65,5 +65,5 @@ def hello():
     '''
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 2000))  # fallback to 5000 if PORT is not set
+    port = int(os.environ.get("PORT", 80))  # fallback to 5000 if PORT is not set
     app.run(host='0.0.0.0', port=port)
